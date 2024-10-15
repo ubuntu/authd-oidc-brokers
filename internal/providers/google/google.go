cd82926d2cebaf6d1a454851da5e2ba5b53657e2
Didier Roche <didier.roche@canonical.com>
2024-10-15 11:25:22 +0200

    google specific provider
    We need to override the generic provider (which should be renamed from
    NoProvider to BaseProvider in the long term) to remove the
    offline_access scope. For TV and limited input devices apps, Google APIs
    will reject any request with this scope, while still allowing non
    interactive user access token refresh.
    All the rest of the functionalities is aligned with the NoProvider one.

---
+++ internal/providers/google/google.go
@@ -0,0 +1,28 @@
+// Package google is the google specific extension.
+package google
+
+import (
+	"github.com/ubuntu/authd-oidc-brokers/internal/providers/noprovider"
+)
+
+// Provider is the google provider implementation.
+type Provider struct {
+	noprovider.NoProvider
+}
+
+// New returns a new GoogleProvider.
+func New() Provider {
+	return Provider{
+		NoProvider: noprovider.New(),
+	}
+}
+
+// AdditionalScopes returns the generic scopes required by the provider.
+// Note that we do not return oidc.ScopeOfflineAccess, as for TV/limited input devices, the API call will fail as not
+// supported by this application type. However, the refresh token will be acquired and is functional to refresh without
+// user interaction.
+// If we start to support other kinds of applications, we should revisit this.
+// More info on https://developers.google.com/identity/protocols/oauth2/limited-input-device#allowedscopes.
+func (Provider) AdditionalScopes() []string {
+	return []string{}
+}
