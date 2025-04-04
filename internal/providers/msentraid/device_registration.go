075b97325e2680b924650ef717da593e944f1350
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-04-04 13:38:38 +0200

    WIP: Device enrollment works (but fetching groups doesn't)

--- internal/providers/msentraid/device_registration.go
+++ internal/providers/msentraid/device_registration.go
@@ -1,25 +1,10 @@
 package msentraid
 
-import (
-	"bytes"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/asn1"
-	"encoding/base64"
-	"encoding/binary"
-	"encoding/pem"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-)
-
 /*
 #cgo LDFLAGS: -lhimmelblau
-
-extern
+#include "himmelblau.h"
+*/
+import "C"
 
 import (
 	"bytes"
@@ -37,6 +22,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"unsafe"
 
 	"github.com/ubuntu/authd/log"
 	"golang.org/x/oauth2"
@@ -56,8 +42,119 @@ func (p Provider) SupportsDeviceRegistration() bool {
 	return true
 }
 
-func (p Provider) RegisterDevice(ctx context.Context, token *oauth2.Token, clientID, tenantID string) error {
+func (p Provider) RegisterDevice(ctx context.Context, token *oauth2.Token, clientID, tenantID, domain string) error {
+	ret := C.set_global_tracing_level(C.TRACE)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to set global tracing level: %d", int(ret))
+	}
 
+	var tpm *C.BoxedDynTpm
+	defer C.tpm_free(tpm)
+	// An optional TPM Transmission Interface. If this parameter is NULL, a Soft Tpm is initialized.
+	var tcti_name *C.char
+	ret = C.tpm_init(tcti_name, &tpm)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to initialize TPM: %d", int(ret))
+	}
+
+	var authValue *C.char
+	defer C.free(unsafe.Pointer(authValue))
+	ret = C.auth_value_generate(&authValue)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to generate auth value: %d", int(ret))
+	}
+
+	var loadableMachineKey *C.LoadableMachineKey
+	defer C.loadable_machine_key_free(loadableMachineKey)
+	ret = C.tpm_machine_key_create(tpm, authValue, &loadableMachineKey)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to create machine key: %d", int(ret))
+	}
+
+	var machineKey *C.MachineKey
+	defer C.machine_key_free(machineKey)
+	ret = C.tpm_machine_key_load(tpm, authValue, loadableMachineKey, &machineKey)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to load machine key: %d", int(ret))
+	}
+
+	var brokerClientApp *C.BrokerClientApplication
+	defer C.broker_free(brokerClientApp)
+	ret = C.broker_init(nil, nil, nil, nil, &brokerClientApp)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to initialize BrokerClientApplication: %d", int(ret))
+	}
+
+	//var deviceAuthResp *C.DeviceAuthorizationResponse
+	//ret = C.broker_initiate_device_flow_for_device_enrollment(brokerClientApp, &deviceAuthResp)
+	//if ret != C.SUCCESS {
+	//	return fmt.Errorf("failed to initiate device flow for device enrollment: %d", int(ret))
+	//}
+	//
+	//var userToken *C.UserToken
+	//ret = C.broker_acquire_token_by_device_flow(brokerClientApp, deviceAuthResp, &userToken)
+	//if ret != C.SUCCESS {
+	//	return fmt.Errorf("failed to acquire token by device flow: %d", int(ret))
+	//}
+
+	//var userToken *C.UserToken
+	//oldRefreshToken := C.CString(token.RefreshToken)
+	//enrollmentScope := C.CString("https://enrollment.manage.microsoft.com/.default")
+	//scopes := []C.CString{enrollmentScope, C.CString("openid"), C.CString("profile"), C.CString("offline_access")}
+	//ret = C.broker_acquire_token_by_refresh_token(
+	//	brokerClientApp,
+	//	oldRefreshToken,
+	//	scopes,
+	//	len(scopes),
+	//)
+
+	//var refreshToken *C.char
+	//defer C.free(unsafe.Pointer(refreshToken))
+	//
+	//ret = C.user_token_refresh_token(userToken, &refreshToken)
+	//if ret != C.SUCCESS {
+	//	return fmt.Errorf("failed to get refresh token: %d", int(ret))
+	//}
+
+	var attrs *C.EnrollAttrs
+	// TODO: Memory leak because attrs are not freed?
+	deviceDisplayName := C.CString("My Ubuntu Device")
+	cDomain := C.CString(domain)
+	ret = C.enroll_attrs_init(
+		cDomain,
+		deviceDisplayName,
+		nil,
+		0,
+		nil,
+		&attrs,
+	)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to initialize enroll attributes: %d", int(ret))
+	}
+
+	var transportKey *C.LoadableMsOapxbcRsaKey
+	defer C.loadable_ms_oapxbc_rsa_key_free(transportKey)
+	var certKey *C.LoadableIdentityKey
+	defer C.loadable_identity_key_free(certKey)
+	var deviceID *C.char
+	defer C.free(unsafe.Pointer(deviceID))
+	ret = C.broker_enroll_device(
+		brokerClientApp,
+		C.CString(token.RefreshToken),
+		attrs,
+		tpm,
+		machineKey,
+		&transportKey,
+		&certKey,
+		&deviceID,
+	)
+	if ret != C.SUCCESS {
+		return fmt.Errorf("failed to enroll device: %d", int(ret))
+	}
+
+	log.Info(ctx, "Enrolled device with ID: %s", C.GoString(deviceID))
+
+	return nil
 }
 
 func (p Provider) RegisterDeviceInGo(ctx context.Context, token *oauth2.Token, clientID, tenantID string) error {
@@ -65,7 +162,7 @@ func (p Provider) RegisterDeviceInGo(ctx context.Context, token *oauth2.Token, c
 	if err != nil {
 		return fmt.Errorf("error acquiring access token for registering device: %w", err)
 	}
-	log.Debugf(ctx, "XXX: Access token for registering device: %s", accessToken)
+	log.Debugf(ctx, "\nXXX: Access token for registering device: %s", accessToken)
 
 	// Fetch nonce from the Nonce Service
 	nonce, err := fetchNonce(tenantID)
@@ -118,7 +215,7 @@ func (p Provider) RegisterDeviceInGo(ctx context.Context, token *oauth2.Token, c
 		"TransportKey": "%s"
 	}`, certificateRequest, deviceDisplayName, deviceType, joinType, osVersion, targetDomain, transportKey)
 
-	log.Debugf(ctx, "XXX: POST %s\n%s", endpoint, reqBody)
+	log.Debugf(ctx, "\nXXX: POST %s┕━%s", endpoint, reqBody)
 
 	req, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBufferString(reqBody))
 	if err != nil {
@@ -367,7 +464,7 @@ func acquireAccessTokenForRegisteringDevice(ctx context.Context, token *oauth2.T
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "application/json")
-	log.Debugf(ctx, "XXX: POST %s: %s", endpoint, payload)
+	log.Debugf(ctx, "\nXXX: POST %s┕━%s", endpoint, payload)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
