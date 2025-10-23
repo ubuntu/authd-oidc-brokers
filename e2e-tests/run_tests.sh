5e57089d0721905c545f4d45a5a896c33ebebb71
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-10-23 12:49:34 +0200

    e2e-runner: Use automatically assigned VNC port
    I encountered the issue that the port 5900 was already in use by another
    VM which caused the tests to fail.
    
    Let libvirt assign a free VNC port instead and tell YARF to use that
    port.

--- e2e-tests/run_tests.sh
+++ e2e-tests/run_tests.sh
@@ -19,6 +19,7 @@ set -eu
 # This script is used to run the YARF tests for the authd-oidc-brokers project.
 ROOT_DIR=$(dirname "$(readlink -f "$0")")
 TESTS_DIR="${ROOT_DIR}/tests"
+VM_NAME=${VM_NAME:-"e2e-runner"}
 
 # Create directory for the test run
 TEST_RUN_DIR="/tmp/e2e-testrun-${BROKER}"
@@ -50,16 +51,17 @@ for test_file in $TESTS_TO_RUN; do
     if [[ "${test_name}" == *"migration"* ]]; then
         SNAPSHOT_NAME=${BROKER}-stable-configured
     fi
-    virsh snapshot-revert e2e-runner "${SNAPSHOT_NAME}" || true
+    virsh snapshot-revert "${VM_NAME}" "${SNAPSHOT_NAME}" || true
 
     echo "Running test: ${test_name}"
     E2E_USER="$E2E_USER" \
     E2E_PASSWORD="$E2E_PASSWORD" \
+    VNC_PORT=$(virsh vncdisplay "${VM_NAME}" | cut -d':' -f2) \
     yarf --outdir "output/${test_name}" --platform=Vnc . || test_result=$? && true
 
     if [ ${test_result} -ne 0 ] && [ -v SNAPSHOT_ON_FAIL ]; then
         echo "Test failed. Saving VM snapshot as requested..."
-        virsh snapshot-create-as e2e-runner "${test_name}-fail-$(date +%Y%m%d%H%M)"
+        virsh snapshot-create-as "${VM_NAME}" "${test_name}-fail-$(date +%Y%m%d%H%M)"
         echo "Snapshot '${test_name}-fail-$(date +%Y%m%d%H%M)' created."
     fi
 
