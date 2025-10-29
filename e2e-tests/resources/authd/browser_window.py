28d436f9c5f708cd74202f46e4eba5b446655556
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-10-29 20:22:05 +0100

    e2e-tests: Set user agent in webview
    I've seen https://microsoft.com/devicelogin consistently fail to load in
    the webview with:
    
        An error occurred while processing your request. Reference
        #97.5d62645f.1761761101.697f9316
        https://errors.edgesuite.net/97.5d62645f.1761761101.697f9316
    
    Changing the user agent did not help, it just worked again after waiting
    an hour.
    
    Nonetheless, we should set a user agent to ensure the missing user agent
    does not cause any issues.

--- e2e-tests/resources/authd/browser_window.py
+++ e2e-tests/resources/authd/browser_window.py
@@ -45,6 +45,9 @@ class BrowserWindow(Gtk.Window):
         self.web_view.get_settings().set_javascript_can_open_windows_automatically(
             False
         )
+        self.web_view.get_settings().set_user_agent(
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:143.0) Gecko/20100101 Firefox/143.0"
+        )
 
         self.web_view.set_can_default(True)
 
