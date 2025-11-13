c5922ad3bc9998e242e49598d67b2471fae0ead8
denisonbarbosa <denison.barbosa@canonical.com>
2025-11-13 11:24:41 -0400

    Allow specifying a specific output dir for the tests
    This makes our life easier when trying to upload the test outputs as
    artifacts on CI.

--- e2e-tests/run-tests.sh
+++ e2e-tests/run-tests.sh
@@ -29,8 +29,9 @@ Options:
   -s, --totp-secret <secret>   Secret to generate OTP codes for the user's MFA (can also be set via TOTP_SECRET environment variable)
   -b, --broker <broker>        Broker to test (can also be set via BROKER environment variable)
   -r, --release <release>      Ubuntu release to test (e.g., 'questing', can also be set via RELEASE environment variable)
-      --rerunfailed            Re-run only the tests that failed in the previous run
+  -o, --output-dir DIR         Directory to store test outputs (default: temporary directory)
   -h, --help                   Show this help message and exit
+      --rerunfailed            Re-run only the tests that failed in the previous run
 EOF
 }
 
@@ -40,6 +41,7 @@ TEST_RUNS_DIR="${XDG_RUNTIME_DIR}/authd-e2e-test-runs"
 
 # Parse command line arguments
 TESTS_TO_RUN=""
+OUTPUT_DIR=""
 while [[ $# -gt 0 ]]; do
     key="$1"
 
@@ -67,6 +69,10 @@ while [[ $# -gt 0 ]]; do
         --rerunfailed)
             RERUNFAILED=1
             shift
+            ;;
+        --output-dir|-o)
+            OUTPUT_DIR="$2"
+            shift 2
             ;;
         -h|--help)
             usage
@@ -129,6 +135,13 @@ TEST_RUN_DIR=$(mktemp -d --tmpdir="${TEST_RUNS_DIR}" "${BROKER}-XXXXXX")
 ln -sf --no-target-directory "${TEST_RUN_DIR}" "${TEST_RUNS_DIR}/${BROKER}-latest"
 cd "${TEST_RUN_DIR}"
 
+if [ -z "${OUTPUT_DIR:-}" ]; then
+    OUTPUT_DIR=output
+    echo "No output directory specified, using current test run directory."
+fi
+
+mkdir -p "${OUTPUT_DIR}" resources
+
 # Activate YARF environment
 YARF_DIR="${ROOT_DIR}/.yarf"
 if [ ! -d "${YARF_DIR}" ]; then
@@ -159,6 +172,7 @@ VNC_PORT="$VNC_PORT" \
 robot \
     --loglevel DEBUG \
     --pythonpath "${YARF_DIR}/yarf/rf_libraries/libraries/vnc" \
+    --outputdir "${OUTPUT_DIR}" \
     "${ROBOT_ARGS[@]}" \
     "$@" \
     tests \
