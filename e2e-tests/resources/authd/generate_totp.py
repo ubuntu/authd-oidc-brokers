a658e041ca6da5cf8e5b7676349e03922803f75e
denisonbarbosa <denison.barbosa@canonical.com>
2026-01-19 07:51:00 -0400

    Add a safety timewindow to TOTP code generator
    The code is generated according to the current time and is valid for 30
    seconds. This means that if we generate the code just before the time
    window changes, it might be invalid by the time we use it.
    To avoid this, we make sure the time is safely within a new window before
    generating the code.

--- e2e-tests/resources/authd/generate_totp.py
+++ e2e-tests/resources/authd/generate_totp.py
@@ -7,7 +7,16 @@ import hmac
 import struct
 import time
 
+TIME_WINDOW = 5
+
 def generate_totp(secret: str) -> str:
+    # The code is generated according to the current time and is valid for 30 seconds.
+    # This means that if we generate the code just before the time window changes,
+    # it might be invalid by the time we use it. To avoid this, we make sure the time
+    # is safely within a new window before generating the code.
+    while time.time() % 30 > (30 - TIME_WINDOW):
+        continue
+
     key = base64.b32decode(secret, True)
     msg = struct.pack(">Q", int(time.time()) // 30)
     hashed_obj = hmac.new(key, msg, hashlib.sha1).digest()
